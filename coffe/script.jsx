7e0bd4b70710eda34d186b63ebc8bce34e242d24
Roman Ahmed <126553951+romanahmed15@users.noreply.github.com>
2024-02-10 12:47:32 +0600

    Add files via upload

---
+++ coffe/script.jsx
@@ -0,0 +1,8 @@
+function imgSlider(anything){
+   document.querySelector('.starbucks').src = anything
+}
+
+function changeColor(color) {
+  const circal = document.querySelector('.crical')
+  circal.style.background = color;
+}
